afd0aee64507cb8f0f6430eb21f2517295c185bb
Nihal kumar singh <ravanwithbabhan@gmail.com>
2025-04-22 19:18:05 +0530

    changes

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Optional: Increase chunk size limit to 1MB
+  }
+});
